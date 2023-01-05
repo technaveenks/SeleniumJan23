a798f3481c6ddf00d7a0e3b6616b50337b458045
naveenakn1 <120537845+naveenakn1@users.noreply.github.com>
2023-01-04 20:01:14 -0500

    package naveena created
    package created to have the code files

---
+++ src/main/java/com/sira/learning/automation/SeleniumJan23/Naveena/App.java
@@ -0,0 +1,13 @@
+package com.sira.learning.automation.SeleniumJan23.Naveena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
